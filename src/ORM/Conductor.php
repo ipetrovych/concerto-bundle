12c02680f4b4f9f2d4eddb71c5c6807891df6bcd
Igor Petrovych <ipetrovych@gmail.com>
2016-01-03 23:25:45 +0200

    Fixed issue with compatibility

--- src/ORM/Conductor.php
+++ src/ORM/Conductor.php
@@ -35,9 +35,30 @@ class Conductor extends EntityManager implements SoloistAwareInterface
      */
     public static function create($conn,  Configuration $config, EventManager $eventManager = null)
     {
+        if ( ! $config->getMetadataDriverImpl()) {
+            throw ORMException::missingMappingDriverImpl();
+        }
+
+        switch (true) {
+            case (is_array($conn)):
+                $conn = \Doctrine\DBAL\DriverManager::getConnection(
+                    $conn, $config, ($eventManager ?: new EventManager())
+                );
+                break;
+
+            case ($conn instanceof Connection):
+                if ($eventManager !== null && $conn->getEventManager() !== $eventManager) {
+                    throw ORMException::mismatchedEventManager();
+                }
+                break;
+
+            default:
+                throw new \InvalidArgumentException("Invalid argument: " . $conn);
+        }
+
         $config->addFilter('soloist', 'Ctrl\Bundle\ConcertoBundle\ORM\Filter\SoloistFilter');
 
-        return parent::create($conn, $config, $eventManager);
+        return new self($conn, $config, $conn->getEventManager());
     }
 
     /**
